05b6f1b21b6a180fcc4d98b453901aa5c884d3df
kimtaewoo <ktw2000111@naver.com>
2025-07-16 15:45:27 +0900

    Spring Boot 프로젝트 생성 with Gradle

---
+++ yogotry/src/main/java/com/yogotry/YogotryApplication.java
@@ -0,0 +1,13 @@
+package com.yogotry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogotryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogotryApplication.class, args);
+	}
+
+}
